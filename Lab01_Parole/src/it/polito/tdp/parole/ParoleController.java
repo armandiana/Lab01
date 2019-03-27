eace0d2806e870992a6cc26bf33958aa1a8dde84
dipic <dipic@DESKTOP-TKN9DBB>
2019-03-27 18:59:47 +0100

    Laboratorio 1 completato

--- Lab01_Parole/src/it/polito/tdp/parole/ParoleController.java
+++ Lab01_Parole/src/it/polito/tdp/parole/ParoleController.java
@@ -33,27 +33,75 @@ public class ParoleController {
     private TextArea txtResult; // Value injected by FXMLLoader
     
     @FXML
+    private TextArea txtTime;
+    
+    @FXML
     private Button btnReset;
+    
+    @FXML
+    private Button btnCancella;
 
     @FXML // fx:id="btnInserisci"
     private Button btnInserisci; // Value injected by FXMLLoader
 
     @FXML
     void doInsert(ActionEvent event) {
-    	// TODO
+    	txtResult.clear(); //ogni volta mi cancella quello che c'è nel box e quindi poi mi stampa in ordine il contenuto dell'elenco
+       
+    	long time=System.nanoTime();
+        
+        String st=txtParola.getText();
+        
+    	 if(st.trim().isEmpty()) {//in questo modo non mi inserisce se lascio vuoto lo spazio e nemmeno se faccio lo spazio e lascio vuoto lo spazio.
+    		 	txtResult.appendText("Parola inserita non valida! Inserire nuovamente\n");
+    		 	txtTime.appendText(Long.toString(System.nanoTime()-time)+"\n");
+    		return;}
+    		 
+    	elenco.addParola(st.trim());
+   
+    	
+    	for(int i=0; i<elenco.getElenco().size(); i++) {
+    		txtResult.appendText(elenco.getElenco().get(i).trim()+"\n");
+    	}
+    	
+    	txtTime.appendText(Long.toString(System.nanoTime()-time)+"\n");
+	
     }
     
     @FXML
     void doReset(ActionEvent event) {
     	// TODO
+    	long time=System.nanoTime();
+    	
+    	elenco.reset();
+    	txtResult.clear();
+    	txtTime.clear();
+    	
+    	txtTime.appendText(Long.toString(System.nanoTime()-time)+"\n");
+    }
+    
+
+    @FXML
+    void doDelete(ActionEvent event) {
+    	long time=System.nanoTime();
+    	String st=txtResult.getSelectedText();
+    	
+    	elenco.delete(st);
+    	
+    	//txtResult.appendText("Parola cancellata: "+st+"\n");
+    	txtTime.appendText(Long.toString(System.nanoTime()-time)+"\n");
+
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
-        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Parole.fxml'.";
-        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Parole.fxml'.";
+    	assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Parole.fxml'.";
         assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Parole.fxml'.";
-
+        assert btnCancella != null : "fx:id=\"btnCancella\" was not injected: check your FXML file 'Parole.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Parole.fxml'.";
+        assert txtTime != null : "fx:id=\"txtTime\" was not injected: check your FXML file 'Parole.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Parole.fxml'.";
+        
         elenco = new Parole() ;
         
     }
--- Lab01_Parole/src/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,36 @@
 package it.polito.tdp.parole.model;
 
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Parole {
+	
+	private List<String>parole;
 		
 	public Parole() {
-		//TODO
+		parole=new LinkedList<String>();
 	}
 	
 	public void addParola(String p) {
-		//TODO
+		//consente di inserire una parola nella struttura dati
+		parole.add(p);
 	}
 	
 	public List<String> getElenco() {
-		//TODO
-		return null;
+		//ritorna l'elenco di parole correnti inserite ordinate alfabeticamente
+		Collections.sort(parole);
+		return parole;
 	}
 	
 	public void reset() {
-		// TODO
+		//elimina tutte le parole dalla struttura dati
+		parole.clear();
+	}
+	
+	public void delete(String st) {
+		//elimina solo una specifica parola che gli passo come paramentro
+		parole.remove(st);
 	}
 
 }
